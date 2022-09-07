28a6a327ac0f8d6671b675733da6a79fbca39414
ryeetc <rclancaster1@gmail.com>
2022-09-06 21:40:54 -0400

    respectfully ask the api to return us one specific warehouse

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -7,4 +7,14 @@ router.get("/", (req,res)=>{
     res.send(JSON.stringify(warehouse))
 })
 
+router.get ("/:warehouseid", (req, res) => {
+    warehouseId = req.params.warehouseid
+    const foundWarehouse = warehouse.find((house)=>{
+        if (house.id === warehouseId) {
+            return house
+        }
+    })
+    res.send(foundWarehouse)
+})
+
 module.exports = router;
