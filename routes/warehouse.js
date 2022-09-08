6d83acfeb434e20c2d2c73a12c7ed8160ba227e6
채언 <Cheeon@JunghoocBookPro.localdomain>
2022-09-08 15:03:03 -0400

    Add post a new warehouse functionality.

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,3 +1,4 @@
+const { randomUUID } = require('crypto');
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
@@ -22,6 +23,32 @@ router.delete ("/:warehouseid", (req, res) => {
     const newWarehouses = warehouse.filter(house => warehouseId !== house.id)
     fs.writeFileSync("./data/warehouses.json", JSON.stringify(newWarehouses))
     res.send("deleted successfully")
+})
+
+router.post("/add", (req, res) => {
+
+    const warehouse = {
+        "id": randomUUID(),
+        "name": req.body.name,
+        "address": req.body.address,
+        "city": req.body.city,
+        "country": req.body.country,
+        "contact": {
+          "name": req.body.contactName,
+          "position": req.body.position,
+          "phone": `+1 ${req.body.phoneNumber}`,
+          "email": req.body.email
+        }
+      }
+
+      console.log(warehouse);
+      const warehouseList = fs.readFileSync("./data/warehouses.json");
+      const warehouseListData = JSON.parse(warehouseList);
+      warehouseListData.push(warehouse)
+      console.log(warehouseListData)
+      fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseListData));
+
+      res.status(201).send(JSON.stringify(warehouse))
 })
 
 module.exports = router;
