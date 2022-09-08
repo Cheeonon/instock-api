093006dd6c39cf2b24a81deabadeb8e88bdaca9c
rowena fonseka <rafonseka@gmail.com>
2022-09-07 23:49:10 -0400

    Inventory List API get endpoint all good

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,28 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const inventory = require('../data/inventories.json')
+const fs = require("fs");
+const inventory = require("../data/inventories.json");
 
 function readInventory() {
-    const inventoryFile = fs.readFileSync('./data/inventories.json')
-    const inventoryData = JSON.parse(inventoryFile)
-    return inventoryData
+  const inventoryFile = fs.readFileSync("./data/inventories.json");
+  const inventoryData = JSON.parse(inventoryFile);
+  return inventoryData;
 }
 
-router.get("/", (req,res)=>{
-    res.send(JSON.stringify(inventory))
-})
+router.get("/", (req, res) => {
+  res.send(JSON.stringify(inventory));
+});
 
-router.get("/:inventoryId", (req,res)=>{
-    const inventoryDetails = readInventory();
+router.get("/:inventoryId", (req, res) => {
+  const inventoryDetails = readInventory();
 
-    const inventoryDetail = inventoryDetails.find((inventory) => {
-        return inventory.id === req.params.inventoryId;
-    })
+  const inventoryDetail = inventoryDetails.find((inventory) => {
+    return inventory.id === req.params.inventoryId;
+  });
 
-    res.send(JSON.stringify(inventoryDetail));
-
-})
-
+  res.send(JSON.stringify(inventoryDetail));
+});
 
 module.exports = router;
