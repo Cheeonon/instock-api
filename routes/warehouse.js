37773bc83c8fd3c6c6e34997b2fa4fdca508a971
채언 <Cheeon@JunghoocBookPro.localdomain>
2022-09-09 13:55:58 -0400

    Add edit function and remove unnecessary json objects

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,54 +1,110 @@
-const { randomUUID } = require('crypto');
-const express = require('express');
+const { randomUUID } = require("crypto");
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const warehouse = require('../data/warehouses.json')
+const fs = require("fs");
+const warehouse = require("../data/warehouses.json");
 
-router.get("/", (req,res)=>{
-    res.send(JSON.stringify(warehouse))
-})
+router.get("/", (req, res) => {
+    res.send(JSON.stringify(warehouse));
+});
 
-router.get ("/:warehouseid", (req, res) => {
-    warehouseId = req.params.warehouseid
-    const foundWarehouse = warehouse.find((house)=>{
+router.get("/:warehouseid", (req, res) => {
+    warehouseId = req.params.warehouseid;
+    const foundWarehouse = warehouse.find((house) => {
         if (house.id === warehouseId) {
-            return house
+            return house;
         }
-    })
-    res.send(foundWarehouse)
-})
+    });
+    res.send(foundWarehouse);
+});
 
-router.delete ("/:warehouseid", (req, res) => {
-    warehouseId = req.params.warehouseid
-    const newWarehouses = warehouse.filter(house => warehouseId !== house.id)
-    fs.writeFileSync("./data/warehouses.json", JSON.stringify(newWarehouses))
-    res.send("deleted successfully")
-})
+router.delete("/:warehouseid", (req, res) => {
+    warehouseId = req.params.warehouseid;
+    const newWarehouses = warehouse.filter((house) => warehouseId !== house.id);
+    fs.writeFileSync("./data/warehouses.json", JSON.stringify(newWarehouses));
+    res.send("deleted successfully");
+});
 
 router.post("/add", (req, res) => {
-
     const warehouse = {
-        "id": randomUUID(),
-        "name": req.body.name,
-        "address": req.body.address,
-        "city": req.body.city,
-        "country": req.body.country,
-        "contact": {
-          "name": req.body.contactName,
-          "position": req.body.position,
-          "phone": `+1 ${req.body.phoneNumber}`,
-          "email": req.body.email
+        id: randomUUID(),
+        name: req.body.name,
+        address: req.body.address,
+        city: req.body.city,
+        country: req.body.country,
+        contact: {
+            name: req.body.contact.name,
+            position: req.body.contact.position,
+            phone: `+1 ${req.body.contact.phone}`,
+            email: req.body.contact.email,
+        },
+    };
+
+    // validation
+    if (
+        req.body.name &&
+        req.body.address &&
+        req.body.city &&
+        req.body.country &&
+        req.body.contact.name &&
+        req.body.contact.position &&
+        req.body.contact.phone &&
+        req.body.contact.email
+    ) {
+        const warehouseList = fs.readFileSync("./data/warehouses.json");
+        const warehouseListData = JSON.parse(warehouseList);
+        warehouseListData.push(warehouse);
+        fs.writeFileSync(
+            "./data/warehouses.json",
+            JSON.stringify(warehouseListData)
+        );
+
+        res.status(201).send(JSON.stringify(warehouse));
+    } else {
+        res.status(400).send({ message: `ERROR BAD REQUEST` });
+    }
+});
+
+router.put("/:warehouseid", (req, res) => {
+    const warehouseList = fs.readFileSync("./data/warehouses.json");
+
+    const newWarehouseList = warehouseList.forEach((warehouse) => {
+        if (warehouse.id === req.params.warehouseid) {
+            warehouse.name = req.body.name;
+            warehouse.address = req.body.address;
+            warehouse.city = req.body.city;
+            warehouse.country = req.body.country;
+            warehouse.contact.name = req.body.name;
+            warehouse.contact.position = req.body.position;
+            warehouse.contact.phone = req.body.phone;
+            warehouse.contact.email = req.body.email;
         }
-      }
+    });
+    
+    // validation
+    if (
+        req.body.name &&
+        req.body.address &&
+        req.body.city &&
+        req.body.country &&
+        req.body.contact.name &&
+        req.body.contact.position &&
+        req.body.contact.phone &&
+        req.body.contact.email
+    ) {
+        const warehouseList = fs.readFileSync("./data/warehouses.json");
+        const warehouseListData = JSON.parse(warehouseList);
+        warehouseListData.push(warehouse);
+        fs.writeFileSync(
+            "./data/warehouses.json",
+            JSON.stringify(warehouseListData)
+        );
 
-      console.log(warehouse);
-      const warehouseList = fs.readFileSync("./data/warehouses.json");
-      const warehouseListData = JSON.parse(warehouseList);
-      warehouseListData.push(warehouse)
-      console.log(warehouseListData)
-      fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseListData));
+        res.status(201).send(JSON.stringify(warehouse));
+    } else {
+        res.status(400).send({ message: `ERROR BAD REQUEST` });
+    }
 
-      res.status(201).send(JSON.stringify(warehouse))
-})
+});
 
 module.exports = router;
